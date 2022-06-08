598791b78022ca60085fe8d867eab916ecf34dbf
GA3RlEL <kruszkagabrys@gmail.com>
2022-06-08 11:09:20 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementsByClassName("hamburger")[0];
+const navMenu = document.getElementsByClassName("nav-menu")[0];
+
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
